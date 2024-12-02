08b4a5b2d7d74833af671b49e2c1f3fde951bef6
nensi <nensirijal61@gmail.com>
2024-12-02 13:52:43 +0800

    allowed hosy issue fix

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--#2x^hdne7vp+ydg15ys5m2fm+--4e+t)us=7$!pc4(3_)8(7h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quiz-project-8lk3gqc8a-nensis-projects-1f47c020.vercel.app']
+ALLOWED_HOSTS = ['quiz-project-api.vercel.app','quizjui.netlify.app']
 
 
 # Application definition
